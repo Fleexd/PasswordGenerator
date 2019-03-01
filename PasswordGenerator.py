5cd17a2e29c69fc4c8e333db6faf234dc5aa365a
Fleexd <48133414+Fleexd@users.noreply.github.com>
2019-03-01 19:13:24 +0200

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,13 +1,13 @@
 import random
-u = input("Enter site name: ")
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+sitename = input("Enter site name: ")
+pwd = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 passlen = 8
 p =  "".join(random.sample(s,passlen ))
 print("Site name: "+u+"\nPassword: "+p +"\n")
 
 
 createfile = open("PasswordManager.txt","a+")
-createfile.write("Site name: "+u+"\nPassword generated: "+p+"\n")
+createfile.write("Site name: "+sitename+"\nPassword generated: "+p+"\n")
 createfile.close()
 
 
