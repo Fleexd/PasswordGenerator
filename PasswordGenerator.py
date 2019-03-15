7c37ac8de79eff95810cd022314ef8b0b4d84e70
FS <48133414+SF1101@users.noreply.github.com>
2019-03-15 17:28:32 +0200

    v2
    Patched the script, changes below:
    - The script is now clearer
    - Added comments that explains what each line does
    - Also added the opportunity to input the chosen username
    - Script creates a stealthier Password manager file, now renamed as "log"

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,13 +1,14 @@
-import random
-sitename = input("Enter site name: ")
-pwd = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-passlen = 8
-p =  "".join(random.sample(s,passlen ))
-print("Site name: "+u+"\nPassword: "+p +"\n")
+import random #Module that makes the random password possible
+sitename = input("Enter site name: ") #User input for the name of the site
+username = input("Chosen username: ") #User input for the desired username
+pwd = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ" #All characters that may be used in the generated password
+passlen = 8 #Length of the password
+p =  "".join(random.sample(pwd,passlen )) #Creating password
+print("Site name: "+sitename+"\nUsername: "+username+"\nPassword: "+p +"\n") #Printing the sites name, username, and generated password
 
 
-createfile = open("PasswordManager.txt","a+")
-createfile.write("Site name: "+sitename+"\nPassword generated: "+p+"\n")
-createfile.close()
+createfile = open("log.log","a+") #Creates a bit stealthier textfile to store all the different credentials for each site.
+createfile.write("Site name: "+sitename+"\nPassword generated: "+p+"\n") #Writes the credentials and site name in an organized way.
+createfile.close() #Closes the file
 
 
